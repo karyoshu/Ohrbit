d606db1a502a27c5a65dff34ccfa10604406904e
Himanshu Mittal <himanshu-mittal@live.com>
2016-04-11 03:14:22 +0530

    Some tweaks

---
+++ Assets/Destroy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("DestroyThis", 3);
+	}
+	
+    void DestroyThis()
+    {
+        Destroy(gameObject);
+    }
+}
